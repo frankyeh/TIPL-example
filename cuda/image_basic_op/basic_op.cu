69154ac3eebd816b0c718331b472cebeb14619e1
frankyeh <frank.yeh@gmail.com>
2022-02-04 15:14:57 -0500

    Update basic_op.cu

--- cuda/image_basic_op/basic_op.cu
+++ cuda/image_basic_op/basic_op.cu
@@ -6,6 +6,21 @@
 
 int main(void)
 {
+    try{
+
+    int nDevices;
+    cudaGetDeviceCount(&nDevices);
+    std::cout << "Device Count:" << nDevices << std::endl;
+    for (int i = 0; i < nDevices; i++) {
+        cudaDeviceProp prop;
+        cudaGetDeviceProperties(&prop, i);
+        printf("Device Number: %d\n", i);
+        printf("  Device name: %s\n", prop.name);
+        printf("  Memory Clock Rate (KHz): %d\n", prop.memoryClockRate);
+        printf("  Memory Bus Width (bits): %d\n", prop.memoryBusWidth);
+        printf("  Peak Memory Bandwidth (GB/s): %f\n\n", 2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+    }
+
     // 1: load atlas (integers representing brain regions)
     tipl::image<3> hfrom;
     if(!hfrom.load_from_file<tipl::io::nifti>("100206_T1w.nii"))
@@ -13,13 +28,13 @@ int main(void)
         std::cout << "cannot find the sample file" << std::endl;
         return 1;
     }
+    std::cout << "Image loaded" << std::endl;
     // enlarge it
     tipl::upsampling(hfrom);
     tipl::upsampling(hfrom);
 
     tipl::image<3> hto(hfrom.shape());
     tipl::device_image<3> dto(hfrom.shape()),dfrom(hfrom);
-
 
     // use single thread
     {
@@ -53,6 +68,13 @@ int main(void)
                to[i] = from[i]*5.5f+100.0f;
         });
         std::cout << cudaGetErrorName(cudaGetLastError()) << std::endl;
+        cudaDeviceSynchronize();
+    }
+
+    }
+    catch(std::runtime_error& er)
+    {
+        std::cout << "ERROR:" << er.what() << std::endl;
     }
 
     return 0;
