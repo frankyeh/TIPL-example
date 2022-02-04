744632e6d9addbe932f094046cd8e28cea8136b6
frankyeh <frank.yeh@gmail.com>
2022-02-03 23:48:29 -0500

    Update linear_registration.cu

--- cuda/image_linear_registration/linear_registration.cu
+++ cuda/image_linear_registration/linear_registration.cu
@@ -19,7 +19,7 @@ int main(void)
     // 2: setup the transformation for linear registration algorithms to solve
 
     tipl::affine_transform<float> affine = {15.0,0,0,0.1f,0.02f,0,1.1f,1.2f,0.95f,0.02f,0,0};
-    std::cout << "ground truth=\n" << affine << std::endl;
+    std::cout << "ground truth=\n" << affine;
 
     // 3: get the transformed image
     tipl::image<3> hto(hfrom.shape());
@@ -31,38 +31,41 @@ int main(void)
     // 4: now use transformed image to calculate the transformation
     bool terminated = false;
     {
+        std::cout << "\ncost function using cpu" << std::endl;
         tipl::reg::mutual_information mi;
         {
             tipl::time t("cpu time for cost function:");
             for(unsigned int i = 0;i < 20;++i)
                 mi(hfrom,hto,trans0);
+            std::cout << "cpu result:" << mi(hfrom,hto,trans0) << std::endl;
         }
-
+        std::cout << "\ncost function using gpu" << std::endl;
         tipl::reg::mutual_information_cuda mi2;
         {
             tipl::time t("gpu time for cost function:");
             for(unsigned int i = 0;i < 20;++i)
                 mi2(hfrom,hto,trans0);
+            std::cout << "gpu result:" << mi2(hfrom,hto,trans0) << std::endl;
         }
 
     }
 
     {
-        // solve using cpu
+        std::cout << "\nsolve using cpu" << std::endl;
         tipl::time t("cpu time (ms):");
         tipl::affine_transform<float> answer;
         tipl::reg::linear_mr<tipl::reg::mutual_information> // use cpu multithread to calculate the cost function
                 (hto,voxel_size,hfrom,voxel_size,answer,tipl::reg::affine,terminated,0.001);
-        std::cout << "cpu answer:\n" << answer << std::endl;
+        std::cout << "cpu answer:\n" << answer;
     }
 
     {
-        // solve using gpu
+        std::cout << "\nsolve using gpu" << std::endl;
         tipl::time t("gpu time (ms):");
         tipl::affine_transform<float> answer;
         tipl::reg::linear_mr<tipl::reg::mutual_information_cuda> // use cuda to calculate the cost function
                 (hto,voxel_size,hfrom,voxel_size,answer,tipl::reg::affine,terminated,0.001);
-        std::cout << "gpu answer:\n" << answer << std::endl;
+        std::cout << "gpu answer:\n" << answer;
         std::cout << cudaGetErrorName(cudaGetLastError()) << std::endl;
     }
     return 0;
