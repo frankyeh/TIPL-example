cec8e52843c052da7c56a052101a435ae6a19485
frankyeh <frank.yeh@gmail.com>
2022-02-09 16:43:18 -0500

    Update nonlinear_registration.cu

--- cuda/image_nonlinear_registration/nonlinear_registration.cu
+++ cuda/image_nonlinear_registration/nonlinear_registration.cu
@@ -80,16 +80,14 @@ int main(void)
     {
         tipl::time t("  accumulate displacement using cpu:");
         float theta = 0.0;
-        tipl::image<3,tipl::vector<3> > out;
-        tipl::reg::cdm_accumulate_dis(dis,dis,out,theta,0.5f);
+        tipl::reg::cdm_accumulate_dis(dis,dis,theta,0.5f);
         std::cout << "  theta=" << theta << std::endl;
     }
 
     {
         tipl::time t("  accumulate displacement using gpu:");
         float theta = 0.0;
-        tipl::device_image<3,tipl::vector<3> > out;
-        tipl::reg::cdm_accumulate_dis_cuda(ddis,ddis,out,theta,0.5f);
+        tipl::reg::cdm_accumulate_dis_cuda(ddis,ddis,theta,0.5f);
         std::cout << "  theta=" << theta << std::endl;
     }
     check_dif(dis,ddis);
